6a57687cd06ceff636a25fca767b8a9c7dcbba39
Julie-lou <137873538+Julie-lou@users.noreply.github.com>
2023-11-23 08:43:09 +0700

    test

--- recursion.java
+++ recursion.java
@@ -1,15 +1,29 @@
 public class recursion {
   public static void main(String[] args) {
-
+        double x = 2.5;
+        int n = 3;
+        double result = power(x, n);
+        System.out.println(x + " raised to the power of " + n + " is: " + result);
     //call your recursion fuction
     //print result
 
    }
     
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
-    }
+    public static double power(double x, int n)
+    {
+        if (n == 0) {
+            return 1;
+        } else if (n > 0) {
+            return x * power(x, n - 1);
+        } else {
+            // Negative power
+            return 1 / power(x, -n);
+        }
+    }}
+  }
+
+    
+    
   
-}
+
     
